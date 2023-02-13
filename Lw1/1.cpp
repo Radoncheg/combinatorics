7af2b017f26b12ec4a413270a944ae4fe8330f72
oleg.oshaev <oshayev@gmail.com>
2023-02-13 20:33:33 +0300

    сдал 1.1

--- Lw1/1.cpp
+++ Lw1/1.cpp
@@ -16,57 +16,15 @@ Visual Studio Professional 2022 (64x) 17.3.4
 
 using namespace std;
 
-vector<size_t> getRandomVector(int size);
-int getRandomNumber(int min, int max);
-double getPermutationDuration(vector<size_t> vector);
-long double fact(int N);
-
-int main()
+long double getPermutationDuration(vector<size_t> vector)
 {
-	int n;
-	char answer;
-	cout << "Please input vector size: ";
-	cin >> n;
-	vector<size_t> v = getRandomVector(n);
-	boost::timer t;
-	t.restart();
-	sort(v.begin(), v.end());
-	cout << "Permutations for vector size " << n << ":" << endl;
-	do
-	{
-		copy(v.begin(), v.end(), ostream_iterator<size_t>(cout, " "));
-		cout << endl;
-	} while (next_permutation(v.begin(), v.end()));
-	double duration = t.elapsed();
 
-	cout << "\n" << "Algorithm Narayana duratuon with output: " << "\n"
-		<< "t(" << n << ") = " << duration << " seconds" << "\n" << "\n"
-		<< "Algorithm Narayana duratuon without output:" << "\n" << "Be patient..."
-		<< "t(10) = " << getPermutationDuration(getRandomVector(10)) << " seconds" << "\n"
-		<< "t(15) = " << getPermutationDuration(getRandomVector(15)) << " seconds" << "\n"
-		<< "t(20) = " << getPermutationDuration(getRandomVector(20)) << " seconds" << "\n" << endl;
-	cout << "T(50) = " << fact(50) << " seconds" << endl;
-	cout << "T(100) = " << fact(100) << " seconds" << endl;
-
-	system("pause");
-}
-
-int getRandomNumber(int min, int max) {
-	return rand() % (max - min + 1) + min;
-}
-
-double getPermutationDuration(vector<size_t> vector)
-{
-	ofstream file("output.txt");
-	file.open("output.txt");
 	boost::timer t;
 	t.restart();
 	std::sort(vector.begin(), vector.end());
 	do
 	{
-		copy(vector.begin(), vector.end(), ostream_iterator<size_t>(file, ""));
 	} while (next_permutation(vector.begin(), vector.end()));
-	file.close();
 	return t.elapsed();
 }
 
@@ -78,10 +36,34 @@ long double fact(int N)
 		return N * fact(N - 1);
 }
 
-vector<size_t> getRandomVector(int size)
+int main()
 {
-	vector<size_t> vec;
-	for (size_t i = 0; i < size; ++i)
-		vec.push_back(getRandomNumber(1, size));
-	return vec;
+	vector<size_t> v = { 1, 2, 3, 4, 5, 6 };
+	vector<size_t> v1 = { 1, 4, 5, 8, 2, 7, 9, 3, 6, 10 };
+	vector<size_t> v2 = { 1, 4, 5, 8, 2, 7, 9, 3, 6, 10, 11, 12 };
+	vector<size_t> v3 = { 1, 4, 5, 8, 2, 7, 9, 3, 6, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+	boost::timer t;
+	t.restart();
+	sort(v.begin(), v.end());
+	cout << "Permutations for vector size " << 6 << ":" << endl;
+	do
+	{
+		copy(v.begin(), v.end(), ostream_iterator<size_t>(cout, " "));
+		cout << endl;
+	} while (next_permutation(v.begin(), v.end()));
+	long double duration = t.elapsed();
+
+	cout << "\n" << "Algorithm Narayana duratuon with output: " << "\n"
+		<< "t(10) = " << duration << " seconds" << "\n" << "\n"
+		<< "Algorithm Narayana duratuon without output:" << "\n" << "Be patient..."
+		<< "t(10) = " << getPermutationDuration(v1) << " seconds" << "\n"
+     	<< "t(15) = " << getPermutationDuration(v2) << " seconds" << "\n"
+	    << "t(20) = " << getPermutationDuration(v3) << " seconds" << "\n" << endl;
+	cout << "T(50) = " << fact(50) << " seconds" << endl;
+	cout << "T(100) = " << fact(100) << " seconds" << endl;
+
+	system("pause");
 }
+
+
+
